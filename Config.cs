6da3a3d967f7388cb1ed4cd6297244a41bc88435
Stsiapan Tsikhonov <stpn.tkhnv@gmail.com>
2021-03-01 14:14:07 +0300

    Init commit

---
+++ Config.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using IdentityServer4;
+using IdentityServer4.Models;
+
+namespace AuthServer
+{
+    public class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+            };
+        
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("socialNetworkApi", "Social Network API")
+            };
+        
+        public static IEnumerable<ApiResource> ApiResources =>
+            new List<ApiResource>
+            {
+                
+                //simple version
+                //TODO rewrite it in expanded version for more control
+                new ApiResource("socialNetworkApi")
+                {
+                    Scopes = { "socialNetworkApi" }
+                }
+            };
+        
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                // React client
+                new Client
+                {
+                    ClientId = "socialNetworkReactClient",
+                    ClientName = "React Client for Social Network",
+                    ClientUri = "http://localhost:3000",
+
+                    AllowedGrantTypes = GrantTypes.Implicit,
+                    
+                    RequireClientSecret = false,
+
+                    RedirectUris =
+                    {                        
+                        "http://localhost:3000/callback",                        
+                    },
+
+                    PostLogoutRedirectUris = { "http://localhost:3000/logout-oidc" },
+                    AllowedCorsOrigins = { "http://localhost:3000" },
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "socialNetworkApi"
+                    },
+
+                    AllowAccessTokensViaBrowser = true
+                }
+            };
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AuthServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //TODO Delete Developer Sign In Credential
+            var builder = services.AddIdentityServer()
+                .AddInMemoryIdentityResources(Config.IdentityResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddInMemoryApiResources(Config.ApiResources)
+                .AddDeveloperSigningCredential();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context => { await context.Response.WriteAsync("Hello World!"); });
+            });
+        }
+    }
+}
