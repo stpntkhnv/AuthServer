b9f6d95bbc8234222595770d0afa8ab3d72ebfe0
Stsiapan Tsikhonov <stpn.tkhnv@gmail.com>
2021-03-01 14:46:11 +0300

    Added AspNetIdentity

---
+++ Data/ApplicationContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data
+{
+    public class ApplicationContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,26 +2,50 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AuthServer.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace AuthServer
 {
     public class Startup
     {
+        private IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
-            //TODO Delete Developer Sign In Credential
-            var builder = services.AddIdentityServer()
+            services.AddControllersWithViews();
+            
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            
+            services.AddIdentity<IdentityUser, IdentityRole>(options =>
+                {
+                    options.User.RequireUniqueEmail = true;
+                })
+                .AddEntityFrameworkStores<ApplicationContext>();
+            
+            var builder = services.AddIdentityServer(options =>
+                {
+                    options.UserInteraction.LoginUrl = "/Auth/Index/";
+                })
+                .AddAspNetIdentity<IdentityUser>()
                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddInMemoryApiScopes(Config.ApiScopes)
                 .AddInMemoryClients(Config.Clients)
-                .AddInMemoryApiResources(Config.ApiResources)
-                .AddDeveloperSigningCredential();
+                .AddInMemoryApiResources(Config.ApiResources);
+
+            builder.AddDeveloperSigningCredential();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -34,6 +58,8 @@ namespace AuthServer
             app.UseRouting();
             
             app.UseIdentityServer();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
